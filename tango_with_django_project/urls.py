d3d9c2a405bc31c1d6b92c50bd4c5072a07ec856
JoakinCC <joakin@ciencias.unam.mx>
2017-10-01 16:35:44 -0500

    creando vistas rango con mapping URLs

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from rango import views
 
 urlpatterns = [
+    url(r'^$',views.index, name='index'),
+    url(r'^rango/', include('rango.urls')),
+    #toda url que empiece con rango
+    #es redirigida a la aplicacion rango
     url(r'^admin/', admin.site.urls),
 ]
