b53ed73982579835e2a1c1641d6e827d9658994b
JoakinCC <joakin@ciencias.unam.mx>
2017-10-20 00:47:00 -0500

    category pages display and slug method

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
+from rango.models import Page
 
 def index(request):
     category_list = Category.objects.order_by('-likes')[:5]
@@ -11,5 +12,24 @@ def index(request):
 def about(request):
     context_dict = {'leader': "Joakin"}
     return render(request, 'rango/about.html', context=context_dict)
+
+def show_category(request, category_name_slug):
+    context_dict = {}
+
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        pages = Page.objects.filter(category=category)
+
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        context_dict['category'] = None
+        context_dict['page'] = None
+
+    return render(request, 'rango/category.html', context_dict)
+
+
+
 
 
