1f7aabb9a99969b31ae591482da96f0eff8abb44
JoakinCC <joakin@ciencias.unam.mx>
2017-11-12 14:05:05 -0600

    register user

--- rango/forms.py
+++ rango/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from rango.models import Category, Page
+from django.contrib.auth.models import User
+from rango.models import Category, Page, UserProfile
+
 
 
 class CategoryForm(forms.ModelForm):
@@ -35,4 +37,17 @@ class PageForm(forms.ModelForm):
 
         exclude = ('category',)
 
+
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
 
